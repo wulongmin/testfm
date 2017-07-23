fb8e6790746faa76eac6b6e3de9ffa1e62a66635
wulongmin <wulm@liguanjia.com>
2017-07-23 11:42:36 +0800

    添加测试文件

---
+++ testfm/Classes/LMTest.h
@@ -0,0 +1,15 @@
+//
+//  LMTest.h
+//  testfm
+//
+//  Created by MacOS on 2017/7/23.
+//  Copyright © 2017年 liguanjia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LMTest : NSObject
+
++ (NSString *)log;
+
+@end
---
+++ testfm/Classes/LMTest.m
@@ -0,0 +1,17 @@
+//
+//  LMTest.m
+//  testfm
+//
+//  Created by MacOS on 2017/7/23.
+//  Copyright © 2017年 liguanjia. All rights reserved.
+//
+
+#import "LMTest.h"
+
+@implementation LMTest
+
++ (NSString *)log {
+    return @"test string";
+}
+
+@end
